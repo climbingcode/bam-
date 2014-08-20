9c764d618a04ed131d3040fe211551bff5442d89
Sunil Unka <sunil.unka@gmail.com>
2014-08-20 21:08:14 +0000

    Fixed ambiguity with brand and user tables. Basic session structure done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
 
+  get 'session/new'
+
+  get 'session/create'
+
+  get 'session/destroy'
+
   resources :users 
 
   resources :brands
@@ -15,6 +21,8 @@ Rails.application.routes.draw do
   resources :guidelines 
 
   resources :misc_assets 
+
+  resource :session, only: [:new, :create, :destroy]
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
