ab73b2a082e3f12feab7983ae8efe3084fc2f714
climbing code <t-j-davis@hotmail.com>
2014-08-20 11:51:08 -0700

    relationships working

--- app/models/brand.rb
+++ app/models/brand.rb
@@ -1,4 +1,9 @@
 class Brand < ActiveRecord::Base
-	has_many :logos, :colors, :fonts, :copies ,:guidelines, :misc_assets
+	has_many :logos
+	has_many :colors
+	has_many :fonts
+	has_many :copies
+	has_many :guidelines
+	has_many :misc_assets
 	has_many :users, through: :user_brands 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ActiveRecord::Base
-	has_many :brands, through: :user_brands  
+	has_many :user_brands
+	has_many :brands, through: :user_brands 
+
 end
