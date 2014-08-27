cd129183ea53f1718313294a1c74af80622a96e7
climbing code <t-j-davis@hotmail.com>
2014-08-26 23:05:30 -0700

    all

--- app/controllers/brand_controller.rb
+++ app/controllers/brand_controller.rb
@@ -11,8 +11,18 @@ class BrandController < ApplicationController
   end
 
 
-  def show
-   
+  def show 
+ 
+    @brand = Brand.find_by(name: params[:id])
+
+    if @brand != nil
+      @colors = @brand.colors
+      @fonts = @brand.fonts 
+      @logos = @brand.logos
+  
+    else 
+      redirect_to '/', notice: "sorry this brand does not exist:("
+    end
   end
 
 
