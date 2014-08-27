96f6c01eb3846f4fc15edb895e73be35c4af5a59
Sunil Unka <sunil.unka@gmail.com>
2014-08-27 20:02:28 +0000

    Completed server side hex to rgb conversion.

--- app/controllers/brands_controller.rb
+++ app/controllers/brands_controller.rb
@@ -106,29 +106,4 @@ class BrandsController < ApplicationController
       params[:brand]
     end
 
-
-    def hex_convert_to_red(hex)
-      hex_red = hex.chomp[0,2]
-      hex_red.to_i(16)
-    end
-
-    def hex_convert_to_green(hex)
-      hex_green = hex.chomp[2,2]
-      hex_green.to_i(16)
-    end
-
-    def hex_convert_to_blue(hex)
-      hex_blue = hex.chomp[4,5]
-      hex_blue.to_i(16)
-    end
-
-    def rgb_concatenate(hex)
-      r = hex_convert_to_red(hex)
-      g = hex_convert_to_green(hex)
-      b = hex_convert_to_blue(hex)
-
-      "#{r}, #{g}, #{b}" 
-    end
-
-
 end
--- app/helpers/brands_helper.rb
+++ app/helpers/brands_helper.rb
@@ -1,2 +1,31 @@
 module BrandsHelper
+
+  def testing(hex)
+    "Hi!" + hex
+  end
+
+
+    def hex_convert_to_red(hex)
+      hex_red = hex.chomp[0,2]
+      hex_red.to_i(16)
+    end
+
+    def hex_convert_to_green(hex)
+      hex_green = hex.chomp[2,2]
+      hex_green.to_i(16)
+    end
+
+    def hex_convert_to_blue(hex)
+      hex_blue = hex.chomp[4,5]
+      hex_blue.to_i(16)
+    end
+
+    def rgb_to_show(hex)
+      r = hex_convert_to_red(hex)
+      g = hex_convert_to_green(hex)
+      b = hex_convert_to_blue(hex)
+
+      "#{r}, #{g}, #{b}" 
+    end
+
 end
